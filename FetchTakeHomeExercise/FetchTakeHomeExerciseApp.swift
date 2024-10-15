b5753d6ee6e7f7b59ee03b2c3f1eab6b8b9bce2f
Andrew Stamm <andrewstamm@me.com>
2024-10-15 17:45:10 -0500

    Initial Commit

---
+++ FetchTakeHomeExercise/FetchTakeHomeExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchTakeHomeExerciseApp.swift
+//  FetchTakeHomeExercise
+//
+//  Created by Andrew Stamm on 10/15/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchTakeHomeExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
